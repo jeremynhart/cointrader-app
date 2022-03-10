5d0264a966ad08dc158ba57c74a7af46000e6fe9
Jeremy Hart <jeremynhart@gmail.com>
2022-03-10 01:10:41 -0600

    Wrapper Styling && WalletConnect Styling @ index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Head from 'next/head'
+import styled from 'styled-components'
 import { useWeb3 } from '@3rdweb/hooks'
 
 export default function Home() {
   const { address, connectWallet } = useWeb3()
 
   return (
-    <div>
+    <Wrapper>
+      <WalletConnect>
       <button onClick={() => connectWallet('injected')}>Connect Wallet</button>
-    </div>
+      </WalletConnect>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  display: grid;
+  place-items: center;
+`
+const WalletConnect = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
